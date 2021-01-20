5229a56974e13e6f7fc18b593b43326f40ff6837
sana026 <73478478+sana026@users.noreply.github.com>
2021-01-20 17:29:00 +0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css';
+import DisplayAllPosts from './components/DisplayAllPosts';
+
+const App = ( ) => {
+  return (
+    <div>
+      <DisplayAllPosts />
+    </div>
+  );
+};
+export default App;
